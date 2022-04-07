88045a8d8509a4e5befd34709ce1f3d84be418db
falovic <fatihsahin.dev@gmail.com>
2022-04-07 17:05:34 +0300

    update

--- todoApp/src/app/app.component.ts
+++ todoApp/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
+  user = 'Fatih';
+  items = [
+{description: "Kahvaltı", action:"No"},
+{description: "Spor", action:"No"},
+{description: "Fatura", action:"No"},
+{description: "Sinema", action:"No"}
+  ];
 }
